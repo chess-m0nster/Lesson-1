eeb0854b93c83db7da95930ac8f106c756cb5005
Viktor Prikolota <49166976+VAmpersand@users.noreply.github.com>
2022-08-15 22:04:06 +0300

    Initial Commit

---
+++ Lesson 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 1
+//
+//  Created by Viktor Prikolota on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
